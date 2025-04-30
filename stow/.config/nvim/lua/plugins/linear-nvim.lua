0143512c592bdb7e3f385cd8414f59be4a046975
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-04-30 14:33:04 -0600

    add linear-nvim

---
+++ stow/.config/nvim/lua/plugins/linear-nvim.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "rmanocha/linear-nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "stevearc/dressing.nvim",
+    },
+    keys = {
+      {
+        "<leader>m",
+        desc = "linear",
+      },
+      {
+        "<leader>mm",
+        function()
+          require("linear-nvim").show_assigned_issues()
+        end,
+        desc = "Linear: Show Assigned Issues",
+      },
+      {
+        "<leader>mc",
+        function()
+          require("linear-nvim").create_issue()
+        end,
+        desc = "Linear: Create Issue",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ms",
+        function()
+          require("linear-nvim").show_issue_details()
+        end,
+        desc = "Linear: Show Issue Details",
+      },
+    },
+  },
+}

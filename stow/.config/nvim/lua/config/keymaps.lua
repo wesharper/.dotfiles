6d56a49df4b1b89cf365a86dd5833333830332c8
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-05-09 16:09:22 -0600

    update keymap

--- stow/.config/nvim/lua/config/keymaps.lua
+++ stow/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- file picker
+vim.keymap.set("n", "<leader><space>", LazyVim.pick("smart"), { desc = "Find Files (smart)" })

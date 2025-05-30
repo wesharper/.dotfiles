e9b2d768ad4149d28070827010999da0448a25d4
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-05-29 19:12:07 -0600

    add dotnet sdk using rpm-ostree

--- scripts/bazzite-config.sh
+++ scripts/bazzite-config.sh
@@ -2,7 +2,7 @@ echo "Installing zsh"
 rpm-ostree install zsh
 
 echo "Installing dotnet SDK (needed for git-credential-manager)"
-rpm-ostree install dotnet-sdk-9.0
+rpm-ostree install dotnet-sdk-8.0
 
 echo "Updating default shell to zsh"
 sudo sed -i 's:/bin/bash:/bin/zsh:g' /etc/passwd

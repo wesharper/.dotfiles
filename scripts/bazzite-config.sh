768611f0c2fe5dc63d4892604191799cc28b4344
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-05-29 19:35:56 -0600

    update bazzite init

--- scripts/bazzite-config.sh
+++ scripts/bazzite-config.sh
@@ -1,9 +1,12 @@
 echo "Installing zsh"
 rpm-ostree install zsh
 
-echo "Installing dotnet SDK (needed for git-credential-manager)"
-rpm-ostree install dotnet-sdk-8.0
-
 echo "Updating default shell to zsh"
 sudo sed -i 's:/bin/bash:/bin/zsh:g' /etc/passwd
 
+echo "Installing dotnet SDK (needed for git-credential-manager)"
+rpm-ostree install dotnet-sdk-8.0
+
+echo "Setting up GPG credential store (needed for git-credential-manager)"
+gpg --gen-key -u "13283469+wesharper@users.noreply.github.com"
+pass init "13283469+wesharper@users.noreply.github.com"

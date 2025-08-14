2c9513bff8ebfa0ecc20bbc82e4cdcf18ee1b0e3
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-08-14 01:24:58 -0600

    cleanup

--- stow/.config/zsh/plugins/blake-street/blake-street.plugin.zsh
+++
@@ -1,4 +0,0 @@
-# Add libpq to path (needed for some ruby gems)
-export PATH="/$HOMEBREW_PREFIX/opt/libpq/bin:$PATH"
-export LDFLAGS="-L/$HOMEBREW_PREFIX/opt/libpq/lib"
-export CPPFLAGS="-I/$HOMEBREW_PREFIX/opt/libpq/include"
--- stow/.config/zsh/plugins/goenv/goenv.plugin.zsh
+++
@@ -1,8 +0,0 @@
-
-if type goenv > /dev/null; then
-  export GOENV_ROOT="$HOME/.goenv"
-  export PATH="$GOENV_ROOT/bin:$PATH"
-  eval "$(goenv init -)"
-  export PATH="$GOROOT/bin:$PATH"
-  export PATH="$PATH:$GOPATH/bin"
-fi

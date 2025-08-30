1bc299d2255dd6b3ac6e27dcdc1a71402df82f71
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-08-30 13:59:58 -0600

    update omarchy init script

--- scripts/omarchy-config.sh
+++ scripts/omarchy-config.sh
@@ -9,7 +9,6 @@ sudo pacman -Syu base-devel procps-ng curl file git
 
 echo "Installing homebrew"
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-
 echo "Configuring homebrew path for bash"
 echo >>~/.bashrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
@@ -19,3 +18,10 @@ echo "Removing conflicting default configurations"
 rm -rf ~/.config/ghostty/
 rm -rf ~/.config/lazygit/
 rm -rf ~/.config/nvim/
+
+echo "Installing git-credential-manager"
+sudo pacman -Syu git-credential-manager
+
+echo "Setting up GPG credential store (needed for git-credential-manager)"
+gpg --gen-key -u "13283469+wesharper@users.noreply.github.com"
+pass init "13283469+wesharper@users.noreply.github.com"

a607c84059ba4af55be033fac13e4a13b8ae2d1b
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-08-30 13:37:25 -0600

    update init script

--- init.sh
+++ init.sh
@@ -1,28 +1,26 @@
+#!/bin/bash
+set -eoux pipefail
+
 if [[ $(uname) = "Darwin" ]]; then
   echo "Configuring OS defaults"
-  sh $(dirname $0)/scripts/darwin-config.sh
+  source ./scripts/darwin-config.sh
 
   echo "Installing homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 fi
 
 if [[ $(uname) = "Linux" ]]; then
   echo "Configuring OS defaults"
-  sh $(dirname $0)/scripts/bazzite-config.sh
+  source ./scripts/omarchy-config.sh
 fi
 
 echo "Installing stow"
 brew install stow
 
 echo "Stowing dotfiles"
-cd $(dirname $0) && stow .
+stow .
 
 echo "Installing software via global Brewfile"
 brew bundle --global
-
-if [[ $(uname) = "Linux" ]]; then
-  echo "Installing git-credential-manager (Linux)"
-  dotnet tool install -g git-credential-manager
-fi
 
 echo "All done!"

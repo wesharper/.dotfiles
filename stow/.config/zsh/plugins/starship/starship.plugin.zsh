1246a180a989b5dce013c5ec31960aa0a98bea29
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-09-24 10:36:18 -0600

    starship workaround

--- stow/.config/zsh/plugins/starship/starship.plugin.zsh
+++ stow/.config/zsh/plugins/starship/starship.plugin.zsh
@@ -3,4 +3,12 @@ then
   curl -sS https://starship.rs/install.sh | sh
 fi
 
+# Check that the function `starship_zle-keymap-select()` is defined.
+# xref: https://github.com/starship/starship/issues/3418
+type starship_zle-keymap-select >/dev/null || \
+  {
+    echo "Load starship"
+    eval "$(/usr/local/bin/starship init zsh)"
+  }
+
 eval "$(starship init zsh)"

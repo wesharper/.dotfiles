7d72b3bcf9ca159abd7143e48ffa7bae28ab7e96
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-10-26 14:28:26 -0600

    update omarchy script

--- scripts/omarchy-config.sh
+++ scripts/omarchy-config.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 set -eoux pipefail
 
-echo "Installing zsh"
-sudo pacman -Syu zsh
-
-echo "Installing homebrew dependencies"
-sudo pacman -Syu base-devel procps-ng curl file git
-
-echo "Installing homebrew"
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-echo "Configuring homebrew path for bash"
-echo >>~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-echo "Installing pass"
-sudo pacman -Syu pass
+echo "Installing essentials"
+sudo pacman -Syu zsh stow
 
 echo "Installing git-credential-manager"
-yay -S git-credential-manager-bin
+yay -S git-credential-manager
 
 echo "Setting up GPG credential store (needed for git-credential-manager)"
 gpg --gen-key -u "13283469+wesharper@users.noreply.github.com"

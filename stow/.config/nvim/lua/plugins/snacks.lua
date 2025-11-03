d6c35a884a7ece23fd668adfe135fdd727f8f37c
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-03 09:54:32 -0700

    update theme config for snacks

--- stow/.config/nvim/lua/plugins/snacks.lua
+++ stow/.config/nvim/lua/plugins/snacks.lua
@@ -2,12 +2,6 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    -- Lazygit theme is configured in the XDG_CONFIG_HOME dir using a custom Catppuccin theme
-    -- this disables the default Snacks theme overrides
-    lazygit = {
-      configure = false,
-      theme = nil,
-    },
     picker = {
       sources = {
         files = {

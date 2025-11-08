4de7337eb7cceff7c4fddeec1fa88afd22b65d3d
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 04:30:10 -0700

    simplify grep

--- scripts/omarchy/gaming/update-environment.sh
+++ scripts/omarchy/gaming/update-environment.sh
@@ -4,14 +4,14 @@ echo "Installing RADV drivers"
 yay -Syu --noconfirm --needed vulkan-radeon mesa
 
 # https://wiki.cachyos.org/configuration/gaming/#increase-maximum-shader-cache-size
-if grep -Fxq "AMD_VULKAN_ICD=RADV" "/etc/environment"; then
+if grep -q "AMD_VULKAN_ICD=RADV" /etc/environment; then
   echo "RADV driver environment variable already set"
 else
   echo "Forcing use of RADV Vulkan driver"
   echo "AMD_VULKAN_ICD=RADV" | sudo tee -a /etc/environment
 fi
 
-if grep -Fxq "MESA_SHADER_CACHE_MAX_SIZE=12G" "/etc/environment"; then
+if grep -q "MESA_SHADER_CACHE_MAX_SIZE=12G" /etc/environment; then
   echo "Shader cache size already increased"
 else
   echo "Increasing shader cache size"

8010127db096d34cf5d34122214461658a9b56f1
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 11:51:50 -0700

    update stow script

--- scripts/omarchy/stow-dotfiles.sh
+++ scripts/omarchy/stow-dotfiles.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Installing stow"
+yay -S --noconfirm --needed stow
+
 echo "Stowing dotfiles (with adopt)"
-stow . --adopt
+if pwd | grep -q .dotfiles; then
+  stow . --adopt
+else
+  echo "You must run the stow command from the root of the .dotfiles directory"
+fi

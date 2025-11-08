21f5f06785d186b19cd6cd707e07b96830a74c12
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-07 17:23:17 -0700

    update omarchy script

--- scripts/omarchy-config.sh
+++ scripts/omarchy-config.sh
@@ -5,9 +5,6 @@ echo "Installing packages"
 sudo pacman -Syu pass steam zsh stow ghostty zsh-syntax-highlighting
 yay -Syu git-credential-manager-bin brave-bin
 
-echo "Interactively remove webapps"
-omarchy-webapp-remove 
-
 echo "Setting up GPG credential store (needed for git-credential-manager)"
 git-credential-manager configure
 gpg --gen-key -u "13283469+wesharper@users.noreply.github.com"

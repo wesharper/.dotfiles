3741d7af9ca7b5fb0e24ab3f342a1427b3d06735
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 11:54:10 -0700

    ensure pwd ends with .dotfiles in stow setup

--- scripts/omarchy/stow-dotfiles.sh
+++ scripts/omarchy/stow-dotfiles.sh
@@ -4,7 +4,7 @@ echo "Installing stow"
 yay -S --noconfirm --needed stow
 
 echo "Stowing dotfiles (with adopt)"
-if pwd | grep -q .dotfiles; then
+if pwd | grep -q '.dotfiles$'; then
   stow . --adopt
 else
   echo "You must run the stow command from the root of the .dotfiles directory"

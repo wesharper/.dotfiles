bdb6d129cfcd946d25bd9942a187ea39d3b73d5a
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 12:07:28 -0700

    reorganize scripts

--- scripts/omarchy/main.sh
+++ scripts/omarchy/main.sh
@@ -6,8 +6,8 @@ script_directory=$(dirname "$0")
 
 "$script_directory"/remove-webapps.sh
 "$script_directory"/configure-terminal.sh
-"$script_directory"/configure-web-browser.sh
 "$script_directory"/configure-github-auth.sh
+"$script_directory"/configure-web-browser.sh
 "$script_directory"/install-additional-packages.sh
 "$script_directory"/stow-dotfiles.sh
 

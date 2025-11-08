835c70487f08b8d900e5cb8d7fabde0af14cf8b6
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 03:39:47 -0700

    update github-auth script

--- scripts/omarchy/configure-github-auth.sh
+++ scripts/omarchy/configure-github-auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ ! $(gpg --list-keys "13283469+wesharper@users.noreply.github.com" &>/dev/null) ]]; then
+if [[ ! $(gpg --list-keys "13283469+wesharper@users.noreply.github.com" 2>/dev/null) ]]; then
   echo "Setting up GPG credential store (needed for git-credential-manager)"
-  gpg --gen-key -u "13283469+wesharper@users.noreply.github.com"
+  gpg --quick-gen-key -u "13283469+wesharper@users.noreply.github.com"
   pass init "13283469+wesharper@users.noreply.github.com"
 fi
 

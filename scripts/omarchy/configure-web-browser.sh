cb5a80c1beac1884d576b4375a84c30a7b9a5373
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 13:17:05 -0700

    use terser language

--- scripts/omarchy/configure-web-browser.sh
+++ scripts/omarchy/configure-web-browser.sh
@@ -7,5 +7,5 @@ echo "Setting Brave as default browser"
 if [[ -f /usr/share/applications/brave-browser.desktop ]]; then
   xdg-settings set default-web-browser brave-browser.desktop
 else
-  echo "Could not configure Brave as default browser, please check installation"
+  echo "Couldn't configure Brave as default browser, check installation"
 fi

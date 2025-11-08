139751c2d822d41800a09fb8113e88c08f6b5a5b
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 14:23:10 -0700

    use 'exit 1'

--- scripts/omarchy/gaming/install-cachyos.sh
+++ scripts/omarchy/gaming/install-cachyos.sh
@@ -8,9 +8,9 @@ tar xvf ~/Downloads/cachyos-repo.tar.xz -C ~/Downloads
 
 # Run the automated installer
 current_dir=$(pwd)
-cd ~/Downloads/cachyos-repo || exit
+cd ~/Downloads/cachyos-repo || exit 1
 
 # the cachyos-repo.sh script assumes that the shell is running from within the directory
 sudo ./cachyos-repo.sh
-cd "$current_dir" || exit
+cd "$current_dir" || exit 1
 

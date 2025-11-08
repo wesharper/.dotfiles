72b8a63f2528f5a5e2a0bf92be52734d0782f35b
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-11-08 13:05:02 -0700

    update main.sh

--- scripts/darwin/main.sh
+++ scripts/darwin/main.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-script_directory=$(dirname "$0")
+if [ -d ~/dev/.dotfiles ]; then
+  echo "Dotfiles repo exists, pulling latest changes"
+  cd ~/dev/.dotfiles || exit 1
+  git pull
+else
+  echo "Cloning dotfiles repo"
+  git clone https://github.com/wesharper/.dotfiles.git ~/dev/.dotfiles
+fi
+
+script_directory=~/dev/.dotfiles/scripts/darwin
 
 "$script_directory"/install-xcode-cli-tools.sh
 "$script_directory"/configure-preferences.sh
--- scripts/omarchy/main.sh
+++ scripts/omarchy/main.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-script_directory=$(dirname "$0")
+if [ -d ~/dev/.dotfiles ]; then
+  echo "Dotfiles repo exists, pulling latest changes"
+  cd ~/dev/.dotfiles || exit 1
+  git pull
+else
+  echo "Cloning dotfiles repo"
+  git clone https://github.com/wesharper/.dotfiles.git ~/dev/.dotfiles
+fi
+
+cd ~/dev/.dotfiles || exit 1
+
+script_directory=~/dev/.dotfiles/scripts/omarchy
 
 "$script_directory"/remove-webapps.sh
 "$script_directory"/configure-terminal.sh

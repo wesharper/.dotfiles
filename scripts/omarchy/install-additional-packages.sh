738b59f9ed524177306da113b051105f27a9ba20
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-12-27 17:07:32 -0700

    update homebrew install step

--- scripts/omarchy/install-additional-packages.sh
+++ scripts/omarchy/install-additional-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "Installing additional applications"
-yay -S --noconfirm --needed fastmail bluetui tmux brew-git
+yay -S --noconfirm --needed fastmail bluetui tmux
 
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

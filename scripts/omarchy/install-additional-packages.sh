606014a68bd5f8471c113eed1868b389cfe285e7
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-12-27 17:10:09 -0700

    update brew setup

--- scripts/omarchy/install-additional-packages.sh
+++ scripts/omarchy/install-additional-packages.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Installing additional applications"
+echo "Installing additional applications and tools"
 yay -S --noconfirm --needed fastmail bluetui tmux
 
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing homebrew recommendations"
+# add homebrew to path
+reload
+brew install gcc

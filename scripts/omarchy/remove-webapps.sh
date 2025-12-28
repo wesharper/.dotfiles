167bf85d10234c7cae96b8d142f0c40b7b786625
Weston Harper <13283469+wesharper@users.noreply.github.com>
2025-12-27 17:00:04 -0700

    remove additional web apps

--- scripts/omarchy/remove-webapps.sh
+++ scripts/omarchy/remove-webapps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Removing unwanted web applications"
-omarchy-webapp-remove Basecamp HEY ChatGPT WhatsApp X
+omarchy-webapp-remove Basecamp HEY ChatGPT WhatsApp X "Google Contacts" "Google Messages" "Google Calendar" YouTube Zoom
 

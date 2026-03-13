dee434837e1f03a47cc32e70dbac5201ab38e379
Weston Harper <13283469+wesharper@users.noreply.github.com>
2026-03-13 15:32:34 -0600

    update nvim config

--- stow/.config/nvim/lua/config/keymaps.lua
+++ stow/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 -- file picker
-vim.keymap.set("n", "<leader><space>", LazyVim.pick("smart"), { desc = "Find Files (smart)" })
+vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })

cc5ed1e2d0111a75f79d921d88c861db52a9a9cb
Weston Harper <13283469+wesharper@users.noreply.github.com>
2026-04-06 09:47:34 -0600

    update formatters

--- .lazy.lua
+++ .lazy.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"prettier",
+				"sqruff",
+			},
+		},
+	},
+	{
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {

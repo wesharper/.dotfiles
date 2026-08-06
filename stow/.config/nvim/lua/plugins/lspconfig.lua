9bba9e6f0f2aaf683c8192f9f8b9133ac382d817
Weston Harper <13283469+wesharper@users.noreply.github.com>
2026-08-06 17:18:50 -0600

    update default lspconfig

--- stow/.config/nvim/lua/plugins/lspconfig.lua
+++ stow/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,27 +1,11 @@
 return {
   {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "svelte-language-server",
-        "sqruff",
-        "tsgo",
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
       servers = {
-        sqruff = {
-          enabled = true,
-        },
         vtsls = {
           enabled = false,
-        },
-        tsgo = {
-          enabled = true,
         },
       },
     },

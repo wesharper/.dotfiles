f53e7d2f36107b6875c4c6fbfc4a406528ed00ae
Weston Harper <13283469+wesharper@users.noreply.github.com>
2026-08-07 13:58:04 -0600

    update lazyvim config

--- stow/.config/nvim/lua/plugins/colorscheme.lua
+++ stow/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
 return {
-  "LazyVim/LazyVim",
-  opts = function(_, opts)
-    if vim.uv.os_uname().sysname == "Darwin" then
-      opts.colorscheme = "catppuccin"
-    end
-    return opts
-  end,
+  {
+    "LazyVim/LazyVim",
+    opts = function(_, opts)
+      if vim.uv.os_uname().sysname == "Darwin" then
+        opts.colorscheme = "catppuccin"
+      end
+      return opts
+    end,
+  },
+  {
+    "RRethy/base16-nvim",
+    config = function()
+      local ok, matugen = pcall(require, "matugen")
+      if ok then
+        matugen.setup()
+      end
+    end,
+  },
 }
